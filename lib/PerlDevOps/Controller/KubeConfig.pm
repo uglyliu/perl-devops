7f8f66dbfa98169a781e9a173f18076fb9cf997c
limengyu1990 <limengyu1990@163.com>
2018-09-23 13:41:19 +0800

    update

--- lib/PerlDevOps/Controller/KubeConfig.pm
+++ lib/PerlDevOps/Controller/KubeConfig.pm
@@ -9,11 +9,6 @@ sub index{
 	 
 }
 
-sub install{
-
-
-}
-
 sub configPage{
 	my $self = shift;
 	#kubeConfig => $self->kubeConfig->find()
@@ -75,9 +70,80 @@ sub _validation {
 	$v->required('loadBalance');
 	$v->required('kubeDesc');
 	$v->required('keepalivedAddress');
-
   	return $v;
 }
+
+
+
+#安装前配置项检测
+sub install_check{
+	my ($self,$k8s_id) = @_;
+	my $kubeConfig = $self->app->kubeConfig->find($k8s_id);
+	return $kubeConfig;
+}
+
+#安装etcd集群
+sub install_etcd{
+
+}
+
+#安装
+sub install{
+	 $self->app->kubeConfig->find();
+
+}
+
+
+#将数组$array转换成以$flag分割字符串
+#默认以空格分割
+sub array2str{
+	my ($array,$flag) = @_;
+	if (ref($array) eq 'ARRAY') {
+		if (!$flag) {
+			$flag = " ";
+		}
+		return join($flag,@$array);
+	}
+}
+
+#配置某个用户的ssh互通
+#$ipstr = 以空格分割ip字符串
+sub ssh_login{
+	
+	my ($user,$passwd,$ipstr) = @_;
+	
+	my $ssh_command = "su - $user -c \"$perl_install_dir/bin/key2nodes -u $user $ipstr\"";
+	
+	$log->info("-------------- 开始批量配置 $user 用户的ssh无密码登录！--------------$ssh_command");
+	
+	my $obj = Expect->spawn($ssh_command) or $log->info("Couldn't exec command:$ssh_command.");
+	
+	my ( $pos, $err, $match, $before, $after ) = $obj->expect(10,
+			[ qr/Password:/i,
+			  sub{ my $self = shift; $self->send("$passwd\r"); exp_continue;}
+			]
+	);
+	$obj->soft_close( );		
+	$log->info("--------------完成批量配置 $user 用户的ssh无密码登录！--------------");
+
+}
+
+#修改系统配置
+sub update_sys_config{
+
+}
+
+#下载相应安装包
+sub download_package{
+
+}
+
+
+#创建CA并产生凭证
+sub create_ca{
+
+}
+
 
 
 
